829e58a9e87e35d5b9b777d94f4f1d0649ec4e23
BhaktiMadiaP <89003552+BhaktiMadiaP@users.noreply.github.com>
2022-08-27 12:01:15 +0530

    .

--- query.js
+++ query.js
@@ -4,12 +4,6 @@ function getParamValue(paramName) {
     console.log("using location",window.location.search);
     var location = window.location.href;
     console.log("Href location",location)
-    // var n = location.indexOf('?');
-    // console.log("Using Splitt",);
-    // console.log("Href n",n)
-    //  location = n != -1 ? location.substring(0, n != -1 ? n : s.length) : location;
-    //  console.log("Href after modification location",location)
-    //  var sublink = location.slice(location.lastIndexOf('/') + 1, location.length);
      
     var s=location.split("?") //split by ?
     // console.log("Href Split location",s)
@@ -28,40 +22,27 @@ function addCode(src) {
       console.log("code added")
 }
 sQuery = getParamValue('sQuery');
-console.log("Query Params",sQuery);
 pId = getParamValue('pId');
-// console.log("Pid", pId);
-// var url = document.getElementById("property").src;
-// console.log(" URLL",url);
-// var embedUrl = document.getElementById("property");
 
-var url ="https://colive-search-system-dev.web.app/smartsearch?redirectHost=https://www.google.com&amp;isMobile=0"
-// console.log("Embed URL", embedUrl)
+var url ="https://colive-search-system-dev.web.app/smartsearch?redirectHost=https://www.google.com&amp;isMobile=1"
 if (sQuery) {
     if (pId) {
         let src = url + '&sQuery=' + sQuery + '&pId=' + pId;
-        // embedUrl.setAttribute('src', src);
         addCode(src);
     }
     else {
         let src = url + '&sQuery=' + sQuery;
-        // embedUrl.setAttribute('src', src);
         addCode(src);
     }
     console.log("If Search Queryyyy", document.getElementById("property"))
 } else {
     if (pId) {
         let src = url + '&pId=' + pId
-        // embedUrl.setAttribute('src', src);
         addCode(src);
     } else {
         let src = url + '&sQuery=pgs in marathalli'
-        // embedUrl.setAttribute('src', src);
         addCode(src);
     }
     console.log("If Noo Search Queryyyy", document.getElementById("property"))
 }
-
-
-// https://colive-search-system-dev.web.app/smartsearch?redirectHost=https://www.google.com&amp;isMobile=0&amp;nQuery=pg" style="width: 100%; height: 3000px" type="text/html"
 
